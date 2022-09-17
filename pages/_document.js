5ba381cca3bde4e6fa3d68471c5a5b75cc7379e8
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2022-09-17 09:39:26 +0300

    import google fonts in _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -11,7 +11,6 @@ export default function Document() {
         rel="stylesheet"
       />
       <body>
-        <Main />
         <NextScript />
       </body>
     </Html>
