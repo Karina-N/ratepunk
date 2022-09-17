9430335dab8b0e84d39525815556e7950f149e39
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2022-09-17 09:32:17 +0300

    initial commit + assets

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import "../styles/main.scss";
+
+export default function MyApp({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
+}
